8f35b6655e694cbd143b8afe4b410ff3e6ab9875
Eneias da Fonseca Dias <edias.dias@terra.com.br>
2025-04-05 16:42:48 -0300

    Corrigindo instalação do @vitejs/plugin-react

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001, // Mudando para 3001 para evitar conflito
+    port: 3001, // Caso queira usar a porta 3001
     host: '0.0.0.0', // Permite que o Vite escute em todas as interfaces de rede
   },
   build: {
-    outDir: 'dist', // Pasta onde os arquivos de build vão ser gerados
+    outDir: 'dist',
   },
 })
