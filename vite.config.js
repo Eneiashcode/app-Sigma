1a2cf216afc60e7ff007d844f9aca325e763657e
Eneias da Fonseca Dias <edias.dias@terra.com.br>
2025-04-05 16:56:32 -0300

    Adicionando domínio do Render à lista de hosts permitidos

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001, // Caso queira usar a porta 3001
+    port: 3000, // Ou 3001, se preferir
     host: '0.0.0.0', // Permite que o Vite escute em todas as interfaces de rede
+    allowedHosts: ['app-sigma.onrender.com'], // Adicione o domínio do Render
   },
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // Pasta onde os arquivos de build vão ser gerados
   },
 })
